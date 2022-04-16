7e3b9e4eef3e00219b8b7d898071ba2ca55e8c37
lcarus-zl <zl351889@163.com>
2022-04-16 16:47:23 +0800

    导航栏布局完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components'
+import {HashRouter as Router,Switch,Route,Link,Redirect} from "react-router-dom";
 
-
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+` 
+const Nav = styled.div`
+  > ul{
+    display: flex;
+    > li{
+      width: 33.3%;
+      text-align: center;
+      padding: 16px;
+    } 
+  }
+`
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from='/' to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <h2>页面不存在</h2>;
+}
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
 }
 
 export default App;
