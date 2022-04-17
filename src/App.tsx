c3af3a5955da3ec6e4bccace14e4989e7e7b7b1a
lcarus-zl <zl351889@163.com>
2022-04-17 14:22:54 +0800

    Money.tsx之TagsSection UI 完成

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,33 @@ import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import NoMatch from 'views/NoMatch';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
+const AppWrapper = styled.div`
+  color: #333;
 `
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-` 
+
 
 function App() {
   return (
-    <Router>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from='/' to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+            <Switch>
+              <Route path="/tags">
+                <Tags/>
+              </Route>
+              <Route path="/money">
+                <Money/>
+              </Route>
+              <Route path="/statistics">
+                <Statistics/>
+              </Route>
+              <Redirect exact from='/' to="/money"/>
+              <Route path="*">
+                <NoMatch/>
+              </Route>
+            </Switch>
+      </Router>
+    </AppWrapper>
+
   );
 }
 
