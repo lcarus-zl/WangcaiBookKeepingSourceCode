8ded64bca997146bbbe1cf35f73bd6f8719c09c5
lcarus-zl <zl351889@163.com>
2022-04-18 15:09:57 +0800

    tagList 样式完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useTags = ()=>{
+const useTags = ()=>{ //封装一个自定义Hook 使用了useState返回一个接口
   const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
   return {tags,setTags}
 }
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,6 +1,40 @@
-import Layout from "components/Layout";
+import Icon from "components/Icon";
 import React from "react";
+import Layout from "components/Layout";
+import styled from "styled-components";
 import { useTags } from "useTags";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li { 
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0 ;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`
+const Center = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Space = styled.div`
+  height: 16px;
+`
 
 
 function Tags() {
@@ -8,11 +42,21 @@ function Tags() {
 
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map( tag => 
-          <li key={tag}>{tag}</li>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right"/>
+          </li>
         )}
-      </ol>
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+        <Space/>
+      </Center>
     </Layout>
   );
 }
