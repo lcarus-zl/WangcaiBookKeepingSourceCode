a936487250932ee5366db511ab0432a1f3daf5b3
lcarus-zl <zl351889@163.com>
2022-04-18 15:42:38 +0800

    创建createId 让id自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,11 +1,12 @@
+import { createId } from "lib/createId";
 import { useState } from "react";
 
 const useTags = ()=>{ //封装一个自定义Hook 使用了useState返回一个接口
   const [tags,setTags] = useState<{id:number,name:string}[]>([
-    {id:1,name:'衣'},
-    {id:2,name:'食'},
-    {id:3,name:'住'},
-    {id:4,name:'行'},
+    {id:createId(),name:'衣'},
+    {id:createId(),name:'食'},
+    {id:createId(),name:'住'},
+    {id:createId(),name:'行'},
   ]);
   return {tags,setTags}
 }
